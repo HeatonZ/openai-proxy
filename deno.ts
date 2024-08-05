98f68979985e164c36c4b2f3761fb572a3228a9f
HeatonZ <905342035@qq.com>
2024-08-05 18:35:01 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -1,4 +1,8 @@
-const OPENAI_API_HOST = "www.u1760361.nyat.app";
+
+import https from "node:https"
+import axios from "npm:axios"
+
+const OPENAI_API_HOST = "cn-sz-yd-plustmp2.natfrp.cloud";
 // const OPENAI_API_HOST = "120.233.26.237";
 const OPENAI_API_PORT = 48787
 
@@ -9,11 +13,21 @@ Deno.serve(async (request) => {
   if(OPENAI_API_PORT){
     url.port = OPENAI_API_PORT
   }
-  const newRequest = new Request(url.toString(), {
-    headers: request.headers,
-    method: request.method,
-    body: request.body,
-    redirect: "follow",
-  });
-  return new Response(await fetch(newRequest));
+  const headers = Object.fromEntries(request.headers)
+  
+  const data = await request.json()
+  return axios({
+            url: url.toString(),
+            method: request.method,
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false
+            }),
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: headers.authorization,
+              "Accept-Encoding": "gzip, deflate",
+              "Connection": 'keep-alive'
+            },
+            data,
+          })
 });
